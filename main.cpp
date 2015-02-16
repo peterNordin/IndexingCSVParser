1bcec03b72086d7bcc91dcbc351f1d5e5c348ae5
Peter Nordin <peterNordin@users.noreply.github.com>
2015-02-16 22:58:23 +0100

    Improvent to example code in main.cpp Added a test file with problematic format (decimal ,) Activated decimal comma replacment with defined macro

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 //!
 
 #include <iostream>
+#define INDCSVP_REPLACEDECIMALCOMMA
 #include "IndexingCSVParser.h"
 
 using namespace std;
@@ -24,54 +25,68 @@ void printVector(vector<T> &rVector)
 
 int main(int argc, char *argv[])
 {
+    string filePath;
     if (argc < 2)
     {
-        cout << "Error to few arguments!" << endl;
-        return 1;
+        filePath = "./Test1_d_snc_c_crlf.csv";
+        cout << "Warning to few arguments! Using default file: " << filePath << endl;
     }
-
-    cout << "Starting with file: " << argv[1] << endl;
-
-    indcsvp::IndexingCSVParser csv;
-    if (!csv.openFile(argv[1]))
+    else
     {
-        cout << "Error; Could not open file: " << argv[1] << endl;
+        filePath = argv[1];
+    }
+    char sepChar = '0';
+    if (argc == 3)
+    {
+        sepChar = string(argv[2])[0];
+    }
+
+    cout << "Using CSV file: " << filePath << endl;
+
+    indcsvp::IndexingCSVParser icsvp;
+    if (!icsvp.openFile(filePath.c_str()))
+    {
+        cout << "Error; Could not open file: " << filePath << endl;
         return 1;
     }
 
-    csv.setCommentChar('#');
+    icsvp.setCommentChar('#');
 
     vector<char> possibleSeparators;
     possibleSeparators.push_back(';');
     possibleSeparators.push_back(',');
-    char sep = csv.autoSetSeparatorChar(possibleSeparators);
-    cout << "Choosing separator char: " << sep << endl;
+    char sep = icsvp.autoSetSeparatorChar(possibleSeparators);
+    cout << "Auto choosing separator char: " << sep << endl;
+    if (sepChar != '0')
+    {
+        icsvp.setSeparatorChar(';');
+    }
+    sepChar = icsvp.getSeparatorChar();
+    cout << "Using separator char: " << sepChar << endl;
 
-    //csv.setSeparatorChar(';');
 
     // Index the file
-    csv.indexFile();
+    icsvp.indexFile();
+    cout << "File: " << filePath << " has nRows: " << icsvp.numRows() << " nCols: " << icsvp.numCols() << " All rows have same num cols (1 = true): " << icsvp.allRowsHaveSameNumCols() << endl;
 
-    cout << "File: " << argv[1] << " has nRows: " << csv.numRows() << " nCols: " << csv.numCols() << " All rows have same num cols (1 = true): " << csv.allRowsHaveSameNumCols() << endl;
-
-    if (csv.numRows() < 100 && csv.numCols() < 100)
+    if (icsvp.numRows() < 100 && icsvp.numCols() < 100)
     {
         cout << "---------- Contents as string (position by position) ----------" << endl;
         bool parseOK;
-        for (size_t r=0; r<csv.numRows(); ++r)
+        for (size_t r=0; r<icsvp.numRows(); ++r)
         {
-            for (size_t c=0; c<csv.numCols(r); ++c)
+            for (size_t c=0; c<icsvp.numCols(r); ++c)
             {
-                cout << csv.getIndexedPosAs<std::string>(r,c, parseOK) << " ";
+                cout << icsvp.getIndexedPosAs<std::string>(r,c, parseOK) << " ";
             }
             cout << endl;
         }
 
         cout << endl << "---------- Fetching all indexed rows ----------" << endl;
-        for (size_t r=0; r<csv.numRows(); ++r)
+        for (size_t r=0; r<icsvp.numRows(); ++r)
         {
             vector<double> row;
-            bool rc = csv.getIndexedRowAs<double>(r, row);
+            bool rc = icsvp.getIndexedRowAs<double>(r, row);
             if (rc)
             {
                 printVector(row);
@@ -82,11 +97,11 @@ int main(int argc, char *argv[])
             }
         }
 
-        cout << endl << "---------- Fetching Rows Columns 0->"<< csv.numCols()/2-1 << " ----------" << endl;
-        for (size_t r=0; r<csv.numRows(); ++r)
+        cout << endl << "---------- Fetching Rows Columns 0->"<< icsvp.numCols()/2-1 << " ----------" << endl;
+        for (size_t r=0; r<icsvp.numRows(); ++r)
         {
             vector<double> row;
-            bool rc = csv.getIndexedRowColumnRangeAs<double>(r, 0, csv.numCols(r)/2, row);
+            bool rc = icsvp.getIndexedRowColumnRangeAs<double>(r, 0, icsvp.numCols(r)/2, row);
             if (rc)
             {
                 printVector(row);
@@ -97,11 +112,11 @@ int main(int argc, char *argv[])
             }
         }
 
-        cout << endl << "---------- Fetching Rows Columns " << csv.numCols()/2 << "->" << csv.numCols()-1 << " ----------" << endl;
-        for (size_t r=0; r<csv.numRows(); ++r)
+        cout << endl << "---------- Fetching Rows Columns " << icsvp.numCols()/2 << "->" << icsvp.numCols()-1 << " ----------" << endl;
+        for (size_t r=0; r<icsvp.numRows(); ++r)
         {
             vector<double> row;
-            bool rc = csv.getIndexedRowColumnRangeAs<double>(r, csv.numCols(r)/2, csv.numCols()-csv.numCols(r)/2, row);
+            bool rc = icsvp.getIndexedRowColumnRangeAs<double>(r, icsvp.numCols(r)/2, icsvp.numCols()-icsvp.numCols(r)/2, row);
             if (rc)
             {
                 printVector(row);
@@ -114,10 +129,10 @@ int main(int argc, char *argv[])
 
 
         cout << endl << "---------- Fetching all indexed columns (printed transposed) ----------" << endl;
-        for (size_t c=0; c<csv.numCols(); ++c)
+        for (size_t c=0; c<icsvp.numCols(); ++c)
         {
             vector<double> col;
-            bool rc = csv.getIndexedColumnAs<double>(c, col);
+            bool rc = icsvp.getIndexedColumnAs<double>(c, col);
             if (rc)
             {
                 printVector(col);
@@ -128,11 +143,11 @@ int main(int argc, char *argv[])
             }
         }
 
-        cout << endl << "---------- Fetching Columns Rows: 0->" << csv.numRows()/2-1 << " (printed transposed) ----------" << endl;
-        for (size_t c=0; c<csv.numCols(); ++c)
+        cout << endl << "---------- Fetching Columns Rows: 0->" << icsvp.numRows()/2-1 << " (printed transposed) ----------" << endl;
+        for (size_t c=0; c<icsvp.numCols(); ++c)
         {
             vector<double> col;
-            bool rc = csv.getIndexedColumnRowRangeAs<double>(c, 0, csv.numRows()/2, col);
+            bool rc = icsvp.getIndexedColumnRowRangeAs<double>(c, 0, icsvp.numRows()/2, col);
             if (rc)
             {
                 printVector(col);
@@ -143,11 +158,11 @@ int main(int argc, char *argv[])
             }
         }
 
-        cout << endl << "---------- Fetching Columns Rows: " << csv.numRows()/2 << "->" << csv.numRows()-1 << " (printed transposed) ----------" << endl;
-        for (size_t c=0; c<csv.numCols(); ++c)
+        cout << endl << "---------- Fetching Columns Rows: " << icsvp.numRows()/2 << "->" << icsvp.numRows()-1 << " (printed transposed) ----------" << endl;
+        for (size_t c=0; c<icsvp.numCols(); ++c)
         {
             vector<double> col;
-            bool rc = csv.getIndexedColumnRowRangeAs<double>(c, csv.numRows()/2, csv.numRows()-csv.numRows()/2 , col);
+            bool rc = icsvp.getIndexedColumnRowRangeAs<double>(c, icsvp.numRows()/2, icsvp.numRows()-icsvp.numRows()/2 , col);
             if (rc)
             {
                 printVector(col);
@@ -161,15 +176,19 @@ int main(int argc, char *argv[])
 
 
         cout << endl << "---------- Get non-indexed rows one by one until EOF ----------" << endl;
-        csv.rewindFile();
-        csv.readUntilData();
-        while (csv.hasMoreDataRows())
+        icsvp.rewindFile();
+        icsvp.readUntilData();
+        while (icsvp.hasMoreDataRows())
         {
             vector<double> data;
-            csv.getRowAs<double>(data);
+            bool isok = icsvp.getRowAs<double>(data);
             for (size_t i=0; i<data.size(); ++i)
             {
                 cout << data[i] << " ";
+            }
+            if (!isok)
+            {
+                cout << "    <<<" << "There were parsing errors on this row";
             }
             cout << endl;
         }
@@ -180,7 +199,7 @@ int main(int argc, char *argv[])
     }
 
 
-    csv.closeFile();
+    icsvp.closeFile();
     cout << endl << "Done!" << endl;
     return 0;
 }
